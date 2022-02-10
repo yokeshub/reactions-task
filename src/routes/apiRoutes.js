e779ac16c29d601182bd18e687a9e253f427e69f
yokeshhub <1794694@tcs.com>
2022-02-10 15:21:52 +0530

    secured set

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -1,4 +1,4 @@
-export const API_ENDPOINT= 'http://artful-iudex.herokuapp.com/'
+export const API_ENDPOINT= 'https://artful-iudex.herokuapp.com/'
 export const API_ROUTES ={
     GET_ALL_REACTIONS:'reactions',
     VIEW_POST_REACTION:'user_content_reactions',
